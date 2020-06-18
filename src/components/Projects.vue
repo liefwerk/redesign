992c6c5bbbadc71096e78e4a3e2cb7edd3cb66f1
mrclasse <natjageni@gmail.com>
2020-06-18 02:29:49 -0300

    init

---
+++ src/components/Projects.vue
@@ -0,0 +1,73 @@
+<template lang="">
+  <div>
+  <p>Hola</p>
+  <canvas class="canvas"></canvas>
+  </div>
+</template>
+
+<script>
+// import { THREE } from 'three'
+// var THREE = require('three')
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    loadObj: function () {
+      var scene = new THREE.Scene()
+      var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+      var intensity = 2
+      var width = 1000
+      var height = 1000
+      var rectLight = new THREE.RectAreaLight(0xffffff, intensity, width, height)
+      rectLight.position.set(50, 50, 0)
+      rectLight.lookAt(0, 0, 0)
+      var renderer = new THREE.WebGLRenderer()
+      var controls = new OrbitControls(camera, renderer.domElement)
+      // controls.update() must be called after any manual changes to the camera's transform
+      camera.position.set(0, 20, 100)
+      controls.update()
+      renderer.setClearColor(0xffE0D3ED, 1)
+      var loader = new GLTFLoader()
+      loader.load('./piggy_girl.gltf', function (gltf) {
+        scene.add(gltf.scene, rectLight)
+      }, undefined, function (error) {
+        console.error(error)
+      })
+
+      camera.position.z = 200
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      document.body.appendChild(renderer.domElement)
+
+      var animate = function () {
+        requestAnimationFrame(animate)
+
+        // cube.rotation.x += 0.01
+        // cube.rotation.y += 0.01
+        // required if controls.enableDamping or controls.autoRotate are set to true
+        controls.update()
+
+        renderer.render(scene, camera)
+      }
+
+      animate()
+    }
+  },
+  beforeMount () {
+    this.loadObj()
+  }
+}
+</script>
+<style lang="css">
+  #canvas{
+    display: block;
+  }
+  *{
+    margin: 0;
+  }
+</style>
